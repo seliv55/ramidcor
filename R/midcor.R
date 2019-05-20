10af448059c1b9646243efde4347e823b594f60d
seliv55 <selivanov@ub.edu>
2019-05-20 19:05:01 +0200

    fix peaks finding

--- R/midcor.R
+++ R/midcor.R
@@ -81,7 +81,7 @@ correct<-function(fn,dfi,mzi,metdat){#fname is the name of file with raw data;
  mzis<-paste(mzi[1:(nfrg+2)])
  mzis[1]<-"Sample_file Max_intensity"
  write(paste('###\t\t\tMIDCOR version 1.0',Sys.time(),'\t***\n'),fn1)
- write("*** MID for each injection, corrected only for natural 13C, 29,30Si, 33,34S ***",fn1)
+ write("*** MID for each injection, corrected only for natural 13C, 29,30Si, 33,34S ***",fn1,append=T)
  write(paste(mzis,collapse=' '),fn1,append=T)
  write.table(cbind(dfi[,1:2],round(fr[,1:(nfrg+1)],4)),fn1,quote=F,append=T,col.names=F, row.names = F);
 # correction
